fbc2cfe75d04e7c1dd1bc5826d6a81f0a62a49f5
houfu <houfu@outlook.sg>
2020-01-27 17:48:27 +0800

    Fix: CSV now includes title

--- pdpc_decisions/save_file.py
+++ pdpc_decisions/save_file.py
@@ -7,7 +7,7 @@ def save_scrape_results_to_csv(options, scrape_results):
     print('Saving scrape results as a csv file.')
     with open(options["csv_path"], 'w', newline='', encoding='utf-8') as f:
         csvwriter = csv.writer(f)
-        csvwriter.writerow(['date', 'respondent', 'summary', 'download_url'])
+        csvwriter.writerow(['date', 'title', 'respondent', 'summary', 'download_url'])
         for result in scrape_results:
-            csvwriter.writerow([result.date, result.respondent, result.summary, result.download_url])
+            csvwriter.writerow([result.date, result.title, result.respondent, result.summary, result.download_url])
     print('Save completed, files saved at ', options["csv_path"])
