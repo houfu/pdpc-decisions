6a9ae3aefb028a0a656672db03b16d374604a07e
Ang Houfu <damon.houf@gmail.com>
2020-03-19 22:19:29 +0800

    Bump to 1.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pdpc-decisions',
-    version='1.0.2',
+    version='1.1.0',
     description='Tools to extract and compile enforcement '
                 'decisions from the Singapore Personal Data Protection Commission',
     author='Ang Houfu ',
