b1238b475fc20959eb17f34e2b240441b0147d06
Ang Houfu <damon.houf@gmail.com>
2020-05-24 14:45:43 +0800

    Bug: Save csv with extra features fails if an item does not have an extra feature.
    Signed-off-by: Ang Houfu <damon.houf@gmail.com>

--- pdpc_decisions/save_file.py
+++ pdpc_decisions/save_file.py
@@ -16,6 +16,16 @@ def save_scrape_results_to_csv(options, scrape_results):
                  'download_url', 'citation', 'case_number',
                  'referred_by', 'referring_to', 'enforcement'])
             for result in scrape_results:
+                if not hasattr(result, 'citation'):
+                    result.citation = ''
+                if not hasattr(result, 'case_number'):
+                    result.case_number = ''
+                if not hasattr(result, 'referring_to'):
+                    result.referring_to = ''
+                if not hasattr(result, 'referred_by'):
+                    result.referred_by = ''
+                if not hasattr(result, 'enforcement'):
+                    result.enforcement = ''
                 csvwriter.writerow(
                     [result.published_date, result.title, result.respondent, result.summary,
                      result.download_url, result.citation, result.case_number,
