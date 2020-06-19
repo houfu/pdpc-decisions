1a3d1efd103d92808c70ba42c6344351df082e15
Ang Houfu <damon.houf@gmail.com>
2020-06-19 22:10:42 +0800

    Fix: Modify scraper_extras.py to use new PDFFile class.
    Signed-off-by: Ang Houfu <damon.houf@gmail.com>

--- pdpc_decisions/scraper_extras.py
+++ pdpc_decisions/scraper_extras.py
@@ -2,17 +2,18 @@
 import logging
 from typing import List
 
-from pdpc_decisions.classes import PDPCDecisionItem
+from pdpc_decisions.classes import PDPCDecisionItem, Options, PDFFile
 from pdpc_decisions.download_file import check_pdf
 
 logger = logging.getLogger(__name__)
 
 
-def get_enforcement(items: List[PDPCDecisionItem]) -> None:
+def get_enforcement(items: List[PDPCDecisionItem], options) -> None:
     """
     Get enforcement details for a list of PDPCDecisionItem.
     The results are saved as the field `enforcement` in the item in the following format.
     (**Type of enforcement**, **the penalty sum for financial penalties**)
+
     :param items: A list of PDPCDecisionItems to be assessed.
     :return: None
     """
@@ -79,33 +80,34 @@ def get_enforcement(items: List[PDPCDecisionItem]) -> None:
                 logger.info(f'Added enforcement <{directions_id}> to {item}')
 
 
-def get_decision_citation_all(items: List[PDPCDecisionItem]) -> None:
+def get_decision_citation_all(items: List[PDPCDecisionItem], options) -> None:
     """
     Gets the citation and case number for a list of PDPCDecisionItem.
     The result is saved in fields "citation" and "case_number" respectively.
+
     :param items:
     :return: None
     """
     logger.info('Adding citation information to items.')
     for item in items:
-        item.citation, item.case_number = get_decision_citation_item(item)
+        item.citation, item.case_number = get_decision_citation_item(item, options)
 
 
-def get_decision_citation_item(source: PDPCDecisionItem) -> (str, str):
+def get_decision_citation_item(source: PDPCDecisionItem, options: Options = None) -> (str, str):
     """
     Gets the citation and case number for a PDPCDecisionItem.
+
+    :param options:
     :param source: The PDPCDecisionItem to get the citation and case number.
     :return: A tuple consisting of (citation, case_number)
     """
     from pdfminer.high_level import extract_text_to_fp
-    import requests
     import io
     import re
-    r = requests.get(source.download_url)
     citation = ''
     case_number = ''
     if check_pdf(source.download_url):
-        with io.BytesIO(r.content) as pdf, io.StringIO() as output_string:
+        with PDFFile(source, options) as pdf, io.StringIO() as output_string:
             extract_text_to_fp(pdf, output_string, page_numbers=[0, 1])
             contents = output_string.getvalue()
         summary_match = re.search(r'SUMMARY OF THE DECISION', contents)
@@ -125,19 +127,20 @@ def get_decision_citation_item(source: PDPCDecisionItem) -> (str, str):
     return citation, case_number
 
 
-def get_case_references_all(items: List[PDPCDecisionItem]) -> None:
+def get_case_references_all(items: List[PDPCDecisionItem], options) -> None:
     """
     Gets the references to cases in each PDPCDecisionItem in items.
     Adds the PDPCDecisionItem to a list of references in a target PDPCDecisionItem.
     The actions are saved in a parameter "referring_to" and "referred_by" of a PDPCDecisionItem respectively.
+
     :param items:
     :return:
     """
     logger.info('Adding case reference information to items.')
     # construct referring to index
     for item in items:
-        citation, _ = get_decision_citation_item(source=item)
-        item.referring_to = get_referring_to_item(item, citation=citation)
+        citation, _ = get_decision_citation_item(source=item, options=options)
+        item.referring_to = get_referring_to_item(item, citation=citation, options=options)
         add_referred_by_to_item(source=item, target=items, citation=citation)
 
 
@@ -145,6 +148,7 @@ def add_referred_by_to_item(source: PDPCDecisionItem, target: List[PDPCDecisionI
     """
     Adds the source PDPCDecisionItem to a PDPCDecisionItem's "referred_by" field in target
     if source makes a reference to an item in target. The method finds references by citation.
+
     :param citation: Citation of source.
     :param source:
     :param target:
@@ -165,9 +169,11 @@ def add_referred_by_to_item(source: PDPCDecisionItem, target: List[PDPCDecisionI
                 result_item.referred_by.append(citation)
 
 
-def get_referring_to_item(source: PDPCDecisionItem, citation: str) -> List[str]:
+def get_referring_to_item(source: PDPCDecisionItem, citation: str , options) -> List[str]:
     """
     Gets the references to cases made in source PDPCDecisionItem.
+
+    :param options:
     :param citation: Citation of source
     :param source:
     :return: A list of case references/citations.
@@ -175,7 +181,7 @@ def get_referring_to_item(source: PDPCDecisionItem, citation: str) -> List[str]:
     referring_to = []
     if check_pdf(source.download_url):
         from .download_file import get_text_from_pdf
-        contents = get_text_from_pdf(source)
+        contents = get_text_from_pdf(source, options)
         import re
         citation_regex = re.compile(r'(\[\d{4}])\s+((?:\d\s+)?[A-Z|()]+)\s+\[?(\d+)\]?')
         citation_matches = citation_regex.finditer(contents)
@@ -187,10 +193,10 @@ def get_referring_to_item(source: PDPCDecisionItem, citation: str) -> List[str]:
     return referring_to
 
 
-def scraper_extras(items):
+def scraper_extras(items, options):
     logging.info('Start adding extra information to items.')
-    get_decision_citation_all(items)
-    get_enforcement(items)
-    get_case_references_all(items)
+    get_decision_citation_all(items, options)
+    get_enforcement(items, options)
+    get_case_references_all(items, options)
     logging.info('End adding extra information to items.')
     return True
